618450c8bcff780bcbbf6d91cc4de9d98e53f7cf
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2024-02-29 11:13:38 +0000

    binarySearch Iterativo

--- src/main/kotlin/search.kt
+++ src/main/kotlin/search.kt
@@ -1,8 +1,22 @@
-fun binarySearch(a: IntArray, l: Int, r: Int, el: Int): Int {
-    if (r < l) return -1
-    val mid = (l+r) / 2
-    return  if (a[mid] == el) mid
+fun binarySearchRecursive(a: IntArray, left: Int, right: Int, element: Int): Int {
+    if (right < left) return -1
+    val mid = (left+right) / 2
+    return  if (a[mid] == element) mid
     else
-        if(a[mid]>el) binarySearch(a,l,mid-1,el)
-        else binarySearch(a,mid+1,r,el)
+        if(a[mid]>element) binarySearchRecursive(a,left,mid-1,element)
+        else binarySearchRecursive(a,mid+1,right,element)
+}
+
+fun binarySearchIterative(a: IntArray, left: Int, right: Int, elem: Int): Int {
+    var l = left
+    var r = right
+    while(l <= r){
+        val mid = (l+r)/2
+        if(a[mid] == elem) return mid
+        else {
+            if(a[mid]<elem) l = mid + 1
+            else r = mid -1
+        }
+    }
+    return -1
 }
